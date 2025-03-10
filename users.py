8c44eba02c7ff831adf0d8fe8ceab2f0d21dab5b
ChrisHonda <christopher.honda92@gmail.com>
2025-03-09 18:33:42 -0600

    Refactor user authentication process to use input prompts and store authenticated users in a dictionary

--- users.py
+++ users.py
@@ -1,10 +1,30 @@
-unauthenticated_users = ['alice', 'bob', 'chris', 'derek', 'elaine']
-authenticated_users = []
+# unauthenticated_users = ['alice', 'bob', 'chris', 'derek', 'elaine']
+# authenticated_users = []
+
+# while unauthenticated_users:
+#     current_user = unauthenticated_users.pop()
+#     print(f"Authenticating {current_user.title()}...")
+#     authenticated_users.append(current_user)
+
+# for user in authenticated_users:
+#     print(f"Authenticated: {user.title()}")
+
+unauthenticated_users = True
+authenticated_users = {}
 
 while unauthenticated_users:
-    current_user = unauthenticated_users.pop()
-    print(f"Authenticating {current_user.title()}...")
-    authenticated_users.append(current_user)
+    name = input("\nWhat is your name? ")
+    response = input (f"Hello {name.title()}, would you like to be authenticated? (yes/no) ")
+    if response == "yes":
+        print(f"Authenticating {name.title()}...")
+        authenticated_users[name] = name.title()
+    else:
+        print(f"Goodbye {name.title()}!")
+    
+    additional_user = input("Would you like to add another user? (yes/no) ")
+    if additional_user.lower() == "no":
+        unauthenticated_users = False
 
-for user in authenticated_users:
-    print(f"Authenticated: {user.title()}")
+    for user in authenticated_users:
+        print(f"Authenticated: {user.title()}")
+
