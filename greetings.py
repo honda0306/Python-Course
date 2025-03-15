68ff33c7e0fdc5dd10bf668fbf49bf320a62cfd1
ChrisHonda <christopher.honda92@gmail.com>
2025-03-14 18:42:48 -0600

    Try defaults and printing results

--- greetings.py
+++ greetings.py
@@ -1,5 +1,5 @@
-def greet_user(username, sentiment):
+def greet_user(username='the one and only gertrude', sentiment='pleasant'):
     """Print simple greeting"""
     print(f"Hi friends, including {username.title()}, I hope you have a {sentiment} day!")
 
-greet_user('tofer', 'tolerable')
+print(greet_user('tofer', 'tolerable'))
