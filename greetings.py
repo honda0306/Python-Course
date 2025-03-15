9912bb1806944a204fc3e4ad97e8186b0d1a42c4
ChrisHonda <christopher.honda92@gmail.com>
2025-03-14 20:52:04 -0600

    Try manipulating lists in a function

--- greetings.py
+++ greetings.py
@@ -1,6 +1,16 @@
-def greet_user(username='the one and only gertrude', sentiment='pleasant'):
-    """Print simple greeting"""
-    print(f"Hi friends, including {username.title()}, I hope you have a {sentiment} day!")
+ungreeted_users = ['alice', 'bob', 'carol', 'dave', 'erin', 'frank', 'grace']
+greeted_users = []
+
+def greet_user(sentiment='pleasant'):
+    """Print simple greeting for each person that hasn't been greeted yet."""
+    while ungreeted_users:
+        current_user = ungreeted_users.pop()
+        print(f"Hi friends, including {current_user.title()}, I hope you have a {sentiment} day!")
+        greeted_users.append(current_user)
+
+    print("\nThe following users have been greeted:")
+    greeted_users.sort()
+    for user in greeted_users:
+        print(user.title())
 
 greet_user()
-greet_user('tofer', 'tolerable')
