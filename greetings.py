c07e504fd8d460b3df381cf06b8b9a325caac3f7
ChrisHonda <christopher.honda92@gmail.com>
2025-03-14 18:48:27 -0600

    Remove redundant print statements

--- greetings.py
+++ greetings.py
@@ -2,4 +2,5 @@ def greet_user(username='the one and only gertrude', sentiment='pleasant'):
     """Print simple greeting"""
     print(f"Hi friends, including {username.title()}, I hope you have a {sentiment} day!")
 
-print(greet_user('tofer', 'tolerable'))
+greet_user()
+greet_user('tofer', 'tolerable')
