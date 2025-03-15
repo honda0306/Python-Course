62594d3761e71ec558cca80efe8e9019e7ee267f
ChrisHonda <christopher.honda92@gmail.com>
2025-03-14 18:28:56 -0600

    Practice positional arguments

--- greetings.py
+++ greetings.py
@@ -1,5 +1,5 @@
-def greet_user(username):
+def greet_user(username, sentiment):
     """Print simple greeting"""
-    print(f"Hi friends, including {username.title()}!")
+    print(f"Hi friends, including {username.title()}, I hope you have a {sentiment} day!")
 
-greet_user('tofer')
+greet_user('tofer', 'tolerable')
