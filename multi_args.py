2d5f625e0c50e278b8de9609bfb5b17423b384dd
ChrisHonda <christopher.honda92@gmail.com>
2025-03-14 21:55:14 -0600

    First pass at functions with arbitrary number of arguments

---
+++ multi_args.py
@@ -0,0 +1,6 @@
+def create_todo_list(*todos):
+    """Create list of items that need to be completed today"""
+    print(todos)
+
+create_todo_list('Start risk assessment', 'Set audit timeline', 'Prepare board presentation')
+create_todo_list('Make cookies', 'Take a nap', 'Go for a walk', 'Read a book')
