eaccdb09d0b59d7c4b590d8bbf18389219f3184a
ChrisHonda <christopher.honda92@gmail.com>
2025-03-14 21:56:10 -0600

    Iterate over todos in more readable format

--- multi_args.py
+++ multi_args.py
@@ -1,6 +1,7 @@
 def create_todo_list(*todos):
     """Create list of items that need to be completed today"""
-    print(todos)
+    for todo in todos:
+        print(f'[] {todo}')
 
 create_todo_list('Start risk assessment', 'Set audit timeline', 'Prepare board presentation')
 create_todo_list('Make cookies', 'Take a nap', 'Go for a walk', 'Read a book')
